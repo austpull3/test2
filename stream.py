841eddfe970a22b692fb5bd122eef47263fbb136
austpull3 <119237117+austpull3@users.noreply.github.com>
2022-12-11 23:09:13 -0800

    Update stream.py

--- stream.py
+++ stream.py
@@ -13,16 +13,6 @@ R packages used:
 - `cowplot`
 ''')
 
-st.subheader('1. Printing text in R')
-with st.expander('See code'):
-  code1 = '''print("Hello world ...")
-  '''
-  st.code(code1, language='R')
-process1 = subprocess.Popen(["Rscript", "helloworld.R"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-result1 = process1.communicate()
-st.write(result1)
-
-
 st.subheader('3. MLB `ggplot2`')
 with st.expander('See code'):
   code3 = '''ggplot() + 
